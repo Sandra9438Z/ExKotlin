429d6a336036bca454636ca6069e727fb0ccbb7a
Sandra9438Z <delblondsandra@gmail.com>
2024-09-10 12:02:38 +0200

    2eme commit

--- 15-Structures_de_controle/15_1-if/P15_1_a-Positif_negatif_nul/src/main/kotlin/main.kt
+++ 15-Structures_de_controle/15_1-if/P15_1_a-Positif_negatif_nul/src/main/kotlin/main.kt
@@ -1,3 +1,15 @@
 fun main() {
     // write your code here
+    var n = 0
+    if (n < 0) {
+        print("Negative")
+    }
+    else if (n > 0){
+        print("Positive")
+    }
+    else if (n == 0){
+    print("Zero")
 }
+}
+
+
--- 15-Structures_de_controle/15_1-if/P15_1_c-nombre_diviseurs/src/main/kotlin/main.kt
+++ 15-Structures_de_controle/15_1-if/P15_1_c-nombre_diviseurs/src/main/kotlin/main.kt
@@ -1,3 +1,13 @@
 fun main() {
     // put your code here
+    var n = readln().toInt()
+        if(n % 2 == 0){
+            println("Divided by 2")
+        }
+        if(n % 3 == 0){
+            println("Divided by 3")
+        }
+        if(n % 6 == 0){
+        println("Divided by 3")
+        }
 }
